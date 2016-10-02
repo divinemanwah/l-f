7b9aa019f59fb7dc503f9867067f10b50dcc1382
Programmer <Programmer>
2016-10-02 11:17:47 +0800

    Fixed server.js

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ var builder = require('botbuilder');
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
+   console.log('port', process.env.port || process.env.PORT || 3978)
 });
   
 // Create chat bot
