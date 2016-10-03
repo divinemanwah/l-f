1baeac68ec16acbcb5b205fb160e9a886c5daf12
Programmer <Programmer>
2016-10-03 15:23:11 +0800

    Fixed server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ bot.dialog('/', new builder.IntentDialog()
 				if(err)
 					session.send('Teka, mejo error. Wait ka lang bes ' + session.message.user.name + '.');
 				else
-					session.send(artist + ' ~ ' + title + '\n---\n' + data.result.response);
+					session.send(artist + ' ~ ' + title + '\n---\n' + data.result.response + '\n' + 'http://api.lololyrics.com/0.5/getLyric?artist=' + encodeURIComponent(artist) + '&track=' + encodeURIComponent(title));
 				
 			});
 		}
@@ -164,7 +164,7 @@ bot.on('conversationUpdate', function (message) {
         }
 
         // Send a goodbye message when bot is removed
-        if (message.membersRemoved) {console.log(message)
+        if (message.membersRemoved) {
             message.membersRemoved.forEach(function (identity) {
                 if (identity.id === message.address.bot.id) {
                     var reply = new builder.Message()
